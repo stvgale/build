9cec11951f6aa6dd36a01f322d5481bfbb465ef7
stevegale <stvgale@gmail.com>
2020-01-09 21:46:51 -0800

    Initial commit

---
+++ precache-manifest.fb5367514b4b250a30f7260d384a3c9d.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8668d283a6d1670ccda21270aad4d8c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0a7a6559603006c1eac",
+    "url": "/static/css/main.54295aed.chunk.css"
+  },
+  {
+    "revision": "f2bfbb0ba11bd0f1713d",
+    "url": "/static/js/2.055330ad.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.055330ad.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a0a7a6559603006c1eac",
+    "url": "/static/js/main.2e329d00.chunk.js"
+  },
+  {
+    "revision": "aa9cd3caf36c26d4f89c",
+    "url": "/static/js/runtime-main.7a9ef57c.js"
+  },
+  {
+    "revision": "3bc6f2c585000f9dc28a175c3bb18202",
+    "url": "/static/media/ash.3bc6f2c5.jpg"
+  },
+  {
+    "revision": "e771c1c80f159b9c789dc099bd3022f4",
+    "url": "/static/media/aspire.e771c1c8.jpg"
+  },
+  {
+    "revision": "047276037157792b1afdd426ff2f24e6",
+    "url": "/static/media/aspire1.04727603.jpg"
+  },
+  {
+    "revision": "91481c568c70c9f4b1125074ab9ffefc",
+    "url": "/static/media/aspire3.91481c56.jpg"
+  },
+  {
+    "revision": "37e1a99d7f4cfbade01db2667199ba8d",
+    "url": "/static/media/aspire4.37e1a99d.jpg"
+  },
+  {
+    "revision": "00cc55619ade46cc599264c963cfbef2",
+    "url": "/static/media/bcd.00cc5561.jpg"
+  },
+  {
+    "revision": "59ff6c4d253395c12e92ea402ffc2421",
+    "url": "/static/media/cover.59ff6c4d.jpg"
+  },
+  {
+    "revision": "376f17cbef175dfd6500f0636a3d4b56",
+    "url": "/static/media/craft.376f17cb.jpg"
+  },
+  {
+    "revision": "9d403476cfa0d0376f05588116dde84a",
+    "url": "/static/media/craft1.9d403476.jpg"
+  },
+  {
+    "revision": "e06be03f87e24ab0e064134b6da97b8c",
+    "url": "/static/media/craft2.e06be03f.jpg"
+  },
+  {
+    "revision": "a7bced34781e7edd1f9977bd8e383428",
+    "url": "/static/media/craft3.a7bced34.jpg"
+  },
+  {
+    "revision": "1673ae81455f5be8fa457358cbc1eae7",
+    "url": "/static/media/craft4.1673ae81.jpg"
+  },
+  {
+    "revision": "1cce36e44b78445f32ed62914f710f5d",
+    "url": "/static/media/craft5.1cce36e4.jpg"
+  },
+  {
+    "revision": "6a2208f9987a5379c5d822e103f1eb9b",
+    "url": "/static/media/craft6.6a2208f9.jpg"
+  },
+  {
+    "revision": "7864ddfdf4ecf104571032b869485313",
+    "url": "/static/media/ikka1.7864ddfd.jpg"
+  },
+  {
+    "revision": "326377bf5ca558b3241e01ded287bc65",
+    "url": "/static/media/ikka2.326377bf.jpg"
+  },
+  {
+    "revision": "aa8a792bbb0d5a5b6b2be648714d9fd4",
+    "url": "/static/media/ikka3.aa8a792b.jpg"
+  },
+  {
+    "revision": "02c5554c1913799b608a6537b712df96",
+    "url": "/static/media/ikka4.02c5554c.jpg"
+  },
+  {
+    "revision": "495744f055a47c12979a162ea0f9bba8",
+    "url": "/static/media/msft.495744f0.jpg"
+  },
+  {
+    "revision": "91b9d1d061a881b591c600bdfde24f8f",
+    "url": "/static/media/trees.91b9d1d0.jpg"
+  }
+]);
